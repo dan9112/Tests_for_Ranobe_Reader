36625222cb1f3f69ef0b0b58d2f615890facf6cd
Danil Bessonov <danbes1997@gmail.com>
2023-10-03 13:30:46 +0300

    инициализация модуля для примера WorkManager

---
+++ work_manager/src/main/kotlin/ru/example/work_manager/MainActivity.kt
@@ -0,0 +1,31 @@
+package ru.example.work_manager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ru.example.work_manager.ui.theme.RanobeReaderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            RanobeReaderTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+private fun MainScreen() {
+
+}
+
+@Preview
+@Composable
+fun MainScreenPreview() = RanobeReaderTheme {
+    MainScreen()
+}
